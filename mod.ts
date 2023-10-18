b20da8fa9e1a65195fb6a433c0cd80f4e7db4e3b
Sam Gwilym <sam@gwil.garden>
2023-10-18 16:33:29 +0200

    Initial commit

---
+++ mod.ts
@@ -0,0 +1,13 @@
+export * from "./src/order/types.ts";
+export * from "./src/order/orders.ts";
+export * from "./src/order/predecessors.ts";
+export * from "./src/order/successors.ts";
+
+export * from "./src/intervals/types.ts";
+export * from "./src/intervals/intervals.ts";
+
+export * from "./src/ranges/types.ts";
+export * from "./src/ranges/ranges.ts";
+
+export * from "./src/products/types.ts";
+export * from "./src/products/products.ts";
